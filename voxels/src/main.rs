11a974988e6a241b1c0f1dae5bfebdba0f9faa2e
Masaru Itoh <156545+voxlet@users.noreply.github.com>
2023-01-31 12:56:49 +0900

    cave chunks

--- voxels/src/main.rs
+++ voxels/src/main.rs
@@ -1,6 +1,7 @@
 use bevy::{diagnostic, prelude::*};
 
 mod camera;
+mod cave;
 mod inspector;
 
 fn main() {
@@ -10,40 +11,12 @@ fn main() {
         .add_plugin(diagnostic::LogDiagnosticsPlugin::default())
         .add_plugin(inspector::InspectorPlugin)
         .add_plugin(camera::CameraPlugin)
+        .add_plugin(cave::CavePlugin)
         .add_startup_system(setup)
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    // add entities to the world
-    let mesh = meshes.add(Mesh::from(shape::Icosphere {
-        radius: 0.45,
-        subdivisions: 32,
-    }));
-
-    for y in -2..=2 {
-        for x in -5..=5 {
-            let x01 = (x + 5) as f32 / 10.0;
-            let y01 = (y + 2) as f32 / 4.0;
-            // sphere
-            commands.spawn_bundle(PbrBundle {
-                mesh: mesh.clone(),
-                material: materials.add(StandardMaterial {
-                    base_color: Color::hex("ffd891").unwrap(),
-                    // vary key PBR parameters on a grid of spheres to show the effect
-                    metallic: y01,
-                    perceptual_roughness: x01,
-                    ..Default::default()
-                }),
-                transform: Transform::from_xyz(x as f32, y as f32 + 0.5, 0.0),
-                ..Default::default()
-            });
-        }
-    }
+fn setup(mut commands: Commands) {
     // light
     commands.spawn_bundle(PointLightBundle {
         transform: Transform::from_translation(Vec3::new(50.0, 50.0, 50.0)),
